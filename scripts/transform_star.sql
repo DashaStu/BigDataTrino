68d383d49d125d57ea6d7b38a7bfdbef64ef30cd
Дарья <ddt0305@yandex.ru>
2026-05-25 00:11:46 +0300

    update

--- scripts/transform_star.sql
+++ scripts/transform_star.sql
@@ -1,57 +1,35 @@
--- 1. Таблица измерений: Продукты
-CREATE TABLE clickhouse.target_db.dim_products AS
-SELECT DISTINCT
-    sale_product_id as product_id, product_name, product_category, product_price, product_brand, product_rating
-FROM (
-    SELECT * FROM postgresql.source_db.public.mock_data
-    UNION ALL
-    SELECT * FROM clickhouse.target_db.mock_data
-);
-
--- 2. Таблица измерений: Клиенты
+DROP TABLE IF EXISTS clickhouse.target_db.dim_customers;
 CREATE TABLE clickhouse.target_db.dim_customers AS
 SELECT DISTINCT
-    sale_customer_id as customer_id, customer_first_name, customer_last_name, customer_country, customer_email
+    customer_first_name, customer_last_name, customer_country, customer_email
 FROM (
-    SELECT * FROM postgresql.source_db.public.mock_data
+    SELECT customer_first_name, customer_last_name, customer_country, customer_email FROM postgresql.public.mock_data
     UNION ALL
-    SELECT * FROM clickhouse.target_db.mock_data
+    SELECT customer_first_name, customer_last_name, customer_country, customer_email FROM clickhouse.target_db.mock_data
 );
 
--- 3. Таблица измерений: Магазины
-CREATE TABLE clickhouse.target_db.dim_stores AS
+DROP TABLE IF EXISTS clickhouse.target_db.dim_products;
+CREATE TABLE clickhouse.target_db.dim_products AS
 SELECT DISTINCT
-    store_name, store_city, store_country, store_email
+    product_name, product_category, CAST(product_price AS DOUBLE) as product_price, product_brand
 FROM (
-    SELECT * FROM postgresql.source_db.public.mock_data
+    SELECT product_name, product_category, product_price, product_brand FROM postgresql.public.mock_data
     UNION ALL
-    SELECT * FROM clickhouse.target_db.mock_data
+    SELECT product_name, product_category, product_price, product_brand FROM clickhouse.target_db.mock_data
 );
 
--- 4. Таблица измерений: Поставщики
-CREATE TABLE clickhouse.target_db.dim_suppliers AS
-SELECT DISTINCT
-    supplier_name, supplier_city, supplier_country
-FROM (
-    SELECT * FROM postgresql.source_db.public.mock_data
-    UNION ALL
-    SELECT * FROM clickhouse.target_db.mock_data
-);
-
--- 5. Таблица фактов: Продажи
+DROP TABLE IF EXISTS clickhouse.target_db.fact_sales;
 CREATE TABLE clickhouse.target_db.fact_sales AS
 SELECT
     id as sale_id,
     date_parse(sale_date, '%m/%d/%Y') as sale_date,
-    sale_product_id as product_id,
-    sale_customer_id as customer_id,
-    store_name, -- используем как ключ
-    supplier_name, -- используем как ключ
-    sale_quantity,
-    sale_total_price,
-    product_rating
+    product_name,
+    customer_email,
+    CAST(sale_quantity AS BIGINT) as sale_quantity,
+    CAST(sale_total_price AS DOUBLE) as sale_total_price,
+    CAST(product_rating AS DOUBLE) as product_rating
 FROM (
-    SELECT * FROM postgresql.source_db.public.mock_data
+    SELECT id, sale_date, product_name, customer_email, sale_quantity, sale_total_price, product_rating FROM postgresql.public.mock_data
     UNION ALL
-    SELECT * FROM clickhouse.target_db.mock_data
+    SELECT id, sale_date, product_name, customer_email, sale_quantity, sale_total_price, product_rating FROM clickhouse.target_db.mock_data
 );
